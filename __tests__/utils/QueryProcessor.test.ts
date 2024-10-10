a8aeefd8dc0e863fee77e69092836411d85c5bdc
Evelynn Chen <evelynnc@andrew.cmu.edu>
2024-10-10 09:47:35 -0400

    answered what is your name

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -23,4 +23,10 @@ describe("QueryProcessor", () => {
         const response: string = QueryProcessor(query);
         expect(response).toBe("My Andrew ID is evelynnc.");
     });
+
+    test('should return player name response', () => {
+        const query = "What is your name?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe("My name is evelynnc.");
+    });
 });
